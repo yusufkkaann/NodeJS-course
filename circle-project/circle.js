0d11bbf49abd5fe6462b7de5317f7704567bab08
Yusuf Kaan <93669959+yusufkkaann@users.noreply.github.com>
2024-05-02 20:19:07 +0300

    Create circle.js

---
+++ circle-project/circle.js
@@ -0,0 +1,10 @@
+function circleArea(r) {
+  return Math.PI * r * r;
+}
+function circleCircumference(r) {
+  return 2 * Math.PI * r;
+}
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
